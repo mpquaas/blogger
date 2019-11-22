90ed17bcf873513b0e54aff4ae203ff74ad8f53b
mpquaas <matthewquaas@unomaha.edu>
2019-11-22 15:45:28 -0600

    selenium install, ats1 skeleton

---
+++ Tests/Login_ATS.py
@@ -0,0 +1,34 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#This will test the login functionality
+class HeyStack_ATS(unittest.TestCase):
+
+   def setUp(self):
+       self.driver = webdriver.Chrome()
+#Remember to create the account for user and pwd before running, otherwise it won't work
+   def test_site(self):
+      #change this value to the desired username
+       user = "instructor"
+      #change this value to the desired password for the above user name
+       pwd = "maverick1a"
+       driver = self.driver
+       driver.maximize_window()
+       driver.get("http://lduman.pythonanywhere.com//admin")
+       elem = driver.find_element_by_id("id_username")
+       elem.send_keys(user)
+       time.sleep(2)
+       elem = driver.find_element_by_id("id_password")
+       elem.send_keys(pwd)
+       time.sleep(2)
+       elem.send_keys(Keys.RETURN)
+       driver.get("http://lduman.pythonanywhere.com/")
+       assert "Logged In"
+       time.sleep(5)
+
+   def tearDown(self):
+       self.driver.close()
+
+if __name__ == "__main__":
+   unittest.main()
